3f6169360570c7e367a12047aeccadc5bd086310
mingyuan-f <1119369173@qq.com>
2021-01-14 23:34:45 +0800

    slice

--- examples/slice.rs
+++ examples/slice.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let string = "hello, world.";
-    let slice = &string[7..];
-    println!("{}", slice.len());
-    println!("{}", slice);
+    let hello_world = "hello, world.";
+    let hello = &hello_world[..5];
+    let world = &hello_world[7..];
+    println!("{}: len: {}", hello, hello.len());
+    println!("{}: len: {}", world, world.len());
+
+    let mut nums = vec![0, 1, 2, 3, 4];
+    let nums_a = &mut nums[0];
+    // let nums_b = &mut nums[1];
+    *nums_a = 10;
 }
