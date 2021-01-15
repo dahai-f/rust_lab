ab5e3b4792d355b4c09958caa6aaf5d68a1ba72b
mingyuan-f <1119369173@qq.com>
2021-01-15 09:37:57 +0800

    thread

--- examples/thread.rs
+++ examples/thread.rs
@@ -4,25 +4,31 @@ use std::sync::{Arc, Mutex, RwLock};
 use std::thread;
 use std::time::Duration;
 
-struct S {
+#[derive(Debug, Clone)]
+struct Value {
     value: i32,
 }
 
-// impl !Send for S {}
-impl !Sync for S {}
+// impl !Send for Value {}
+// impl !Sync for Value {}
 
-fn do_f(f: impl FnOnce()) {
-    f()
+struct S<'v> {
+    value: Value,
+    ref_value: &'v Value,
 }
 
+static VALUE: Value = Value { value: 100 };
+
 fn main() {
-    let mut s = S { value: 10 };
-    do_f(|| s.value = 100);
-    println!("{}", s.value);
+    let mut s = S {
+        value: Value { value: 10 },
+        ref_value: &VALUE,
+    };
+    println!("{:?}", s.value);
 
     // let rw_s = Arc::new(RwLock::new(s));
     let t1 = thread::spawn(move || {
-        println!("{}", s.value);
+        println!("{:?}", s.value);
 
         for i in 1..10 {
             println!("Greeting {} from other thread!", i);
