b3e6dadae2bdf5b2a549438dccdcf7927321baa7
dahai-f <1119369173@qq.com>
2021-05-15 09:44:56 +0800

    serde lab

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,123 @@
-fn main() {
-    use serde::*;
-    use serde_traitobject as s;
-    use std::any::Any;
+use std::fmt::Debug;
+use std::marker::PhantomData;
 
-    #[derive(Serialize, Deserialize, Debug)]
-    struct MyStruct {
-        foo: String,
-        bar: usize,
+use serde::ser::SerializeTuple;
+use serde::*;
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MyStruct {
+    foo: String,
+    bar: usize,
+}
+
+trait MyTrait: Debug {
+    fn change(&mut self, new_value: usize);
+    fn ser(&self) -> serde_json::Value;
+}
+
+impl MyTrait for MyStruct {
+    fn change(&mut self, new_value: usize) {
+        self.bar = new_value + new_value;
     }
 
-    trait MyTrait: Any {}
+    fn ser(&self) -> Value {
+        serde_json::to_value(self).unwrap()
+    }
+}
 
-    impl MyTrait for MyStruct {}
+fn de(value: Value) -> Box<dyn MyTrait> {
+    let value: MyStruct = serde_json::from_value(value).unwrap();
+    Box::new(value)
+}
 
-    let my_struct = MyStruct {
-        foo: String::from("abc"),
-        bar: 123,
+#[derive(Debug)]
+struct MyTraitBox(Box<dyn MyTrait>);
+
+impl MyTraitBox {
+    fn new(my_trait: impl 'static + MyTrait) -> Self {
+        Self(Box::new(my_trait))
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Message {
+    s: Vec<MyTraitBox>,
+}
+
+impl Serialize for MyTraitBox {
+    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
+    where
+        S: Serializer,
+    {
+        let mut tuple: <S as Serializer>::SerializeTuple = serializer.serialize_tuple(2)?;
+        tuple.serialize_element(std::any::type_name::<MyStruct>())?;
+        tuple.serialize_element(&self.0.ser())?;
+        tuple.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for MyTraitBox {
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        use std::*;
+
+        struct Visitor;
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = MyTraitBox;
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                write!(formatter, "a MyTraitBox")
+            }
+            #[inline]
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                let t0: String = match seq.next_element()? {
+                    Some(value) => value,
+                    None => return Err(serde::de::Error::invalid_length(0, &self)),
+                };
+                println!("{}", t0);
+                let t1: Value = match seq.next_element()? {
+                    Some(value) => value,
+                    None => return Err(serde::de::Error::invalid_length(1, &self)),
+                };
+                Ok(MyTraitBox(de(t1)))
+            }
+        }
+        deserializer.deserialize_tuple(2, Visitor)
+    }
+}
+
+fn main() {
+    let message = Message {
+        s: vec![
+            MyTraitBox::new(MyStruct {
+                foo: String::from("a"),
+                bar: 1,
+            }),
+            MyTraitBox::new(MyStruct {
+                foo: String::from("b"),
+                bar: 2,
+            }),
+            MyTraitBox::new(MyStruct {
+                foo: String::from("c"),
+                bar: 3,
+            }),
+            MyTraitBox::new(MyStruct {
+                foo: String::from("d"),
+                bar: 4,
+            }),
+        ],
     };
 
-    let erased: s::Box<dyn s::Any> = s::Box::new(my_struct);
-
-    let serialized = serde_json::to_string(&erased).unwrap();
-    let deserialized: s::Box<dyn MyTrait> = serde_json::from_str(&serialized).unwrap();
-
-    let downcast: Box<MyStruct> = Box::<dyn Any>::downcast(deserialized.into_any()).unwrap();
-
-    println!("{:?}", downcast);
+    let serialized = serde_json::to_string(&message).unwrap();
+    println!("{}", serialized);
+    std::fs::write("./resources/message.json", serialized).unwrap();
+    // let message = std::fs::read("./resources/message.json").unwrap();
+    let deserialized: Message = serde_json::from_str(&serialized.unwrap()).unwrap();
+    println!("{:?}", deserialized);
     // MyStruct { foo: "abc", bar: 123 }
 }
