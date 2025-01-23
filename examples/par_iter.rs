be31e14a921cfc1a1bcb932e4208997010ed6909
eranfu <eranfu@tencent.com>
2025-01-23 10:45:23 +0800

    par_iter

--- examples/par_iter.rs
+++ examples/par_iter.rs
@@ -6,5 +6,10 @@ fn main() {
     vec1.par_iter()
         .zip(vec2.par_iter())
         .flat_map(|(a, b)| vec![a, b])
+        .filter_map(|&v| if v >= 2 { Some(v * 2) } else { None })
         .for_each(|v| println!("{v}"));
+
+    let s = "hello";
+    let ss: String = s.par_chars().filter_map(|c| Some(c)).collect();
+    println!("{ss}");
 }
